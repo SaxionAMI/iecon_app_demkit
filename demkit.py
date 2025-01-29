aa9ffc66d8b69b641070f6280a5de2f02137905f
Javier FG <j.ferreiragonzalez@saxion.nl>
2025-01-29 20:47:34 +0100

    Updated way to define the model to run, now it is possible to set it via config.yml instead of Dockerfile.

--- demkit.py
+++ demkit.py
@@ -22,122 +22,116 @@ import sys
 from conf.usrconf import load_demkit_config
 
 sys.stderr.write("_________________________________________________________________________________________________\n")
-sys.stderr.write('      --- IECON Application DEMKit ---\n')
+sys.stderr.write('      --- IECON Application DEMKit EMS ---\n')
 sys.stderr.write('       Based on DEMKIT version 2023.3\n\n')
-sys.stderr.write('Copyright 2023 University of Twente, Enschede, the Netherlands\n\n')
-sys.stderr.write('Licensed under the Apache License, Version 2.0 (the "License")\n')
+sys.stderr.write('Copyright 2023 University of Twente, Enschede, the Netherlands\n')
+sys.stderr.write('Copyright 2025 Saxion University of Applied Sciences, Enschede, the Netherlands\n')
+sys.stderr.write('\nLicensed under the Apache License, Version 2.0 (the "License")\n')
 sys.stderr.write("_________________________________________________________________________________________________\n")
 sys.stderr.flush()
 
-if len(sys.argv) > 1:
+# Move to the application folder
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+app_path = os.getcwd()  # Save the current application path
 
-    # Move to the application folder
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    app_path = os.getcwd()  # Save the current application path
+sys.stderr.write(f"Current working directory: {os.getcwd()} " + "\n")
 
-    sys.stderr.write(f"Current working directory: {os.getcwd()} " + "\n")
+# Add config path to the system
+sys.path.insert(0, os.path.join(app_path, 'conf/'))  # Add to system path
 
-    # Add config path to the system
-    sys.path.insert(0, os.path.join(app_path, 'conf/'))  # Add to system path
+# Load config file
+from conf.usrconf import demCfg
 
-    # Load config file
-    from conf.usrconf import demCfg
-
-    try:
-        if demCfg['ver'] < 4:
-            sys.stderr.write(
-                "[ERROR] Incorrect configuration version found. Make sure to have a proper conf/usrconf.py file!"
-                " Please refer to the provided exanple.\n")
-            sys.stderr.flush()
-            exit()
-
-        if demCfg['ver'] < 4.1:
-            sys.stderr.write(
-                "\n[WARNING] Old configuration version detected."
-                " Please refer to the usrconf.py.misc example file to see the changes.\n"
-            )
-            sys.stderr.write(
-                "[WARNING] DEMKit will use the default values for missing configuration entries"
-                " but manual configuration is advised.\n"
-            )
-            sys.stderr.write("[WARNING] Current expected config version is: 4.1.\n")
-            sys.stderr.write("[WARNING] Changelog:  \n")
-            sys.stderr.write("[WARNING] Version 4.1 adds: demCfg['var'] entries and demCfg['timezone'] entry. \n\n")
-            sys.stderr.flush()
-
-            # Variable output for logs ans backups (stored within the workspace folder of a model by default)
-            demCfg['var'] = {}
-            demCfg['var']['backup'] = os.path.join(app_path, "var/backup/")
-            demCfg['var']['databasebackup'] = os.path.join(app_path, "var/backup/database/")
-            demCfg['var']['log'] = os.path.join(app_path, "var/log/")
-
-            # Timezone information
-            from pytz import timezone
-
-            demCfg['timezonestr'] = 'Europe/Amsterdam'
-            demCfg['timezone'] = timezone(demCfg['timezonestr'])
-
-        # Add trailing slash
-        if demCfg['env']['path'][-1] != '\\' and demCfg['env']['path'][-1] != '/':
-            demCfg['env']['path'] += '/'
-        if demCfg['workspace']['path'][-1] != '\\' and demCfg['workspace']['path'][-1] != '/':
-            demCfg['workspace']['path'] += '/'
-
-    except:
+try:
+    if demCfg['ver'] < 4:
         sys.stderr.write(
-            "Errors occurred when loading the configuration file. Make sure to have a proper conf/usrconf.py file!"
-            " Please refer to the provided exanple.\n"
-        )
+            "[ERROR] Incorrect configuration version found. Make sure to have a proper conf/usrconf.py file!"
+            " Please refer to the provided exanple.\n")
         sys.stderr.flush()
         exit()
 
-    sys.stderr.write('pyDEM directory: ' + demCfg['env']['path'] + '\n')
-    sys.stderr.write('User model directory: ' + demCfg['workspace']['path'] + '\n')
+    if demCfg['ver'] < 4.1:
+        sys.stderr.write(
+            "\n[WARNING] Old configuration version detected."
+            " Please refer to the usrconf.py.misc example file to see the changes.\n"
+        )
+        sys.stderr.write(
+            "[WARNING] DEMKit will use the default values for missing configuration entries"
+            " but manual configuration is advised.\n"
+        )
+        sys.stderr.write("[WARNING] Current expected config version is: 4.1.\n")
+        sys.stderr.write("[WARNING] Changelog:  \n")
+        sys.stderr.write("[WARNING] Version 4.1 adds: demCfg['var'] entries and demCfg['timezone'] entry. \n\n")
+        sys.stderr.flush()
+
+        # Variable output for logs ans backups (stored within the workspace folder of a model by default)
+        demCfg['var'] = {}
+        demCfg['var']['backup'] = os.path.join(app_path, "var/backup/")
+        demCfg['var']['databasebackup'] = os.path.join(app_path, "var/backup/database/")
+        demCfg['var']['log'] = os.path.join(app_path, "var/log/")
+
+        # Timezone information
+        from pytz import timezone
+
+        demCfg['timezonestr'] = 'Europe/Amsterdam'
+        demCfg['timezone'] = timezone(demCfg['timezonestr'])
+
+    # Add trailing slash
+    if demCfg['env']['path'][-1] != '\\' and demCfg['env']['path'][-1] != '/':
+        demCfg['env']['path'] += '/'
+    if demCfg['workspace']['path'][-1] != '\\' and demCfg['workspace']['path'][-1] != '/':
+        demCfg['workspace']['path'] += '/'
+
+except:
+    sys.stderr.write(
+        "Errors occurred when loading the configuration file. Make sure to have a proper conf/usrconf.py file!"
+        " Please refer to the provided exanple.\n"
+    )
     sys.stderr.flush()
+    exit()
 
-    # Load the DEM platform
-    sys.path.insert(0, os.path.join(app_path, demCfg['env']['path']))
+sys.stderr.write('pyDEM directory: ' + demCfg['env']['path'] + '\n')
+sys.stderr.write('User model directory: ' + demCfg['workspace']['path'] + '\n')
+sys.stderr.flush()
 
-    modelPath = demCfg['workspace']['path']
-    try:
-        modelName = demCfg['model']['name']
-    except:
-        modelName = ""
+# Load the DEM platform
+sys.path.insert(0, os.path.join(app_path, demCfg['env']['path']))
 
-    # Get arguments:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--folder')
-    parser.add_argument('-m', '--model')
-    parser.add_argument('-s', '--socket')
-    parser.add_argument('-u', '--smarthouseusb')
+modelPath = demCfg['workspace']['path']
+try:
+    modelName = demCfg['model']['name']
+except:
+    modelName = ""
 
-    # Parse arguments
-    args = parser.parse_args()
-    if args.folder:
-        if (args.folder[:1] == "/") or (args.folder[:2] == "~/"):
-            modelPath = args.folder
-        else:
-            modelPath += args.folder
-    if args.model:
-        modelName = args.model
-    if args.socket:
-        demCfg['network']['sockPath'] = args.socket
-    if args.smarthouseusb:
-        demCfg['smarthouse']['usb'] = args.smarthouseusb
+# Get arguments:
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--folder')
+parser.add_argument('-m', '--model')
+parser.add_argument('-s', '--socket')
+parser.add_argument('-u', '--smarthouseusb')
 
-    sys.stderr.write('Loading model: ' + modelName + ' from ' + modelPath + '\n')
-    sys.stderr.flush()
+# Parse arguments
+args = parser.parse_args()
+if args.folder:
+    if (args.folder[:1] == "/") or (args.folder[:2] == "~/"):
+        modelPath = args.folder
+    else:
+        modelPath += args.folder
+if args.model:
+    modelName = args.model
+if args.socket:
+    demCfg['network']['sockPath'] = args.socket
+if args.smarthouseusb:
+    demCfg['smarthouse']['usb'] = args.smarthouseusb
 
-    # import the model path
-    sys.path.insert(0, os.path.join(app_path, modelPath))
+sys.stderr.write('Loading model: ' + modelName + ' from ' + modelPath + '\n')
+sys.stderr.flush()
 
-    # change the working directory to the model directory
-    os.chdir(modelPath)
+# import the model path
+sys.path.insert(0, os.path.join(app_path, modelPath))
 
-    # Load the desired model
-    importlib.import_module(modelName)
+# change the working directory to the model directory
+os.chdir(modelPath)
 
-else:
-    sys.stderr.write("Usage:" + '\n')
-    sys.stderr.write('demkit.py -f <folder> -m <model> [-d <database>] [-c] [-s <socket>] [-u <smarthouseusb>]' + '\n')
-    sys.stderr.flush()
+# Load the desired model
+importlib.import_module(modelName)
